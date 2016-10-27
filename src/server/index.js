b227e00122a7eb932d41bbe8634b308111148c77
Derek Miller <derekmiller9@gmail.com>
2016-10-27 12:56:54 -0700

    File uploaded from local server to S3 bucket #1

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,54 @@
 const express = require('express');
 const multer = require('multer')
+const s3 = require('s3')
 
 const upload = multer({ dest: './uploads/'});
 const PORT = 3001;
 
 const app = express();
 
+const videoPaths = [];
+
+
 app.post('/api/form-upload', upload.single('video'), (req, res) => {
+
+  const videoPaths = [];
+  videoPaths.push(req.file.path)
+
+  const client = s3.createClient({
+    maxAsyncS3: 20,
+    s3RetryCount: 3,
+    s3RetryDelay: 1000,
+    multipartUploadThreshold: 125829120,
+    multipartUploadSize: 104857600,
+    s3Options: {
+      accessKeyId: "",
+      secretAccessKey: "",
+    },
+  })
+
+  const params = {
+    localFile: '/Users/derekmiller/project3/' + videoPaths[0],
+
+    s3Params : {
+      Bucket: "game-bias-videos",
+      Key: req.file.filename
+    },
+  };
+
+  var uploader = client.uploadFile(params);
+  uploader.on('error', function(err) {
+    console.error("unable to upload:", err.stack);
+  });
+  uploader.on('progress', function() {
+    console.log("progress", uploader.progressMd5Amount,
+    uploader.progressAmount, uploader.progressTotal);
+  });
+  uploader.on('end', function() {
+    console.log("done uploading");
+  });
+
+
   res.status(204);
   res.send();
 })
