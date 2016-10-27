89a8d9cbdf5cbd1143231b609f82dff6e4046f60
Derek Miller <derekmiller9@gmail.com>
2016-10-27 09:49:18 -0700

    Updated video upload form using Fetch API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-const angular = require('angular');
+const form = document.getElementById('upload-video')
 
-const myApp = angular.module('app', [])
+form.addEventListener('submit', theEvent => {
+  theEvent.preventDefault();
+
+  const video = document.getElementById('video').files[0]
+  const name = document.getElementById('name').value
+  const description = document.getElementById('description').value
+  const game = document.getElementById('game').value
+
+
+  const formData = new FormData();
+  formData.append('video', video, name)
+  formData.append('name', name)
+  formData.append('description', description)
+  formData.append('game', game)
+
+  fetch('api/form-upload', {
+    method: 'POST',
+    body: formData
+  })
+
+})
