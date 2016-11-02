112b08bbdf52212364eb19502d4b05ccf65483fc
Derek Miller <derekmiller9@gmail.com>
2016-11-01 17:29:15 -0700

    Created controller for dashbaord view. #3

---
+++ src/dashboard.js
@@ -0,0 +1,7 @@
+module.exports = dashboardController;
+
+function dashboardController() {
+
+  const vm =this;
+
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const angular = require('angular');
 
-
 angular
   .module('app', [require('angular-route')])
   .config(config)
-  .controller('FormController', require('./form'));
-
+  .controller('FormController', require('./form'))
+  .controller('DashboardController', require('./dashboard'));
 
 function config($routeProvider) {
   $routeProvider
   .when('/upload', {
-    templateUrl: 'upload.html'
+    templateUrl: 'upload.html',
+    controller: 'FormController'
   })
   .when('/', {
-    templateUrl: 'dashboard.html'
+    templateUrl: 'dashboard.html',
+    controller: 'DashboardController'
   })
 }
