03a03c200d0f5eeff6cdeff1d8cc5b1a64327733
Derek Miller <derekmiller9@gmail.com>
2016-11-01 17:42:09 -0700

    Created GET  request for loading all videos upon view dashboard. #3

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,7 +1,18 @@
 module.exports = dashboardController;
 
-function dashboardController() {
+dashboardController.$inject = ['$http']
+
+function dashboardController($http) {
 
   const vm =this;
+
+  vm.videos = [];
+  init()
+
+  function init() {
+    $http.get('api/all-videos').then(res => {
+      vm.videos.push(res.data)
+    })
+  }
 
 }
