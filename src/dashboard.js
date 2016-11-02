25aa4ce0708b965cfae5ae8e0718ac3e190da5b1
Derek Miller <derekmiller9@gmail.com>
2016-11-02 09:45:40 -0700

    Used ng-repeat to display list of all uploaded videos. #3

--- src/dashboard.js
+++ src/dashboard.js
@@ -7,12 +7,14 @@ function dashboardController($http) {
   const vm =this;
 
   vm.videos = [];
+
   init()
 
   function init() {
     $http.get('api/all-videos').then(res => {
-      vm.videos.push(res.data)
+      vm.videos = res.data
     })
   }
+
 
 }
