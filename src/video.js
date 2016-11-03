bf11948fc1799c97966373dacc58a92635065f1b
Derek Miller <derekmiller9@gmail.com>
2016-11-03 10:03:46 -0700

    Draw charts of four main emotions. #2

--- src/video.js
+++ src/video.js
@@ -1,3 +1,6 @@
+const Highcharts = require('highcharts');
+require('highcharts/modules/exporting')(Highcharts);
+
 module.exports = videoController
 
 videoController.$inject = ['$http', '$routeParams']
@@ -6,14 +9,66 @@ function videoController($http, $routeParams) {
 
   const vm = this
 
-  const video = {};
+  const video = {}
+  let data = [];
 
   init();
-
   function init() {
     $http.get('api/single-video/' + $routeParams.filename ).then(res => {
       vm.video = res.data[0]
+      setData(vm.video, 'joy', 'yellow');
+      setData(vm.video, 'sadness', 'purple');
+      setData(vm.video, 'fear', 'red');
+      setData(vm.video, 'surprise', 'blue');
     })
+  }
+
+
+  function setData(video, emotion, color){
+    data = video.analytics.frames.map((frame) => [
+      frame.time,
+      frame.people[0].emotions[emotion]
+    ])
+    createChart(emotion, color);
+  }
+
+
+  function createChart(emotion, color) {
+    Highcharts.chart(emotion, {
+      chart: {
+        type: 'spline'
+      },
+      title: {
+        text: ''
+      },
+      xAxis: {
+        title: {
+          text: 'Time (milliseconds)'
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Emotional Intensity (0-100)'
+        },
+        min: 0
+      },
+      plotOptions: {
+        spline: {
+          lineWidth: 4,
+          color: color,
+          marker: {
+            enabled: false
+          }
+        }
+      },
+      series: [{
+        name: emotion,
+        data: data
+      }]
+
+    })
+
+
   }
 
 }
